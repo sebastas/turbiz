c26c99307dc57684368b6a4605e6d3840b5dcad8
sebastas <seb.slettebakken@gmail.com>
2019-02-21 14:54:38 +0100

    login med redirect til hjem

--- src/index.js
+++ src/index.js
@@ -2,80 +2,59 @@ import * as React from 'react';
 import { Component } from 'react-simplified';
 import ReactDOM from 'react-dom';
 import { NavLink, HashRouter, Route } from 'react-router-dom';
-import { studentService } from './services';
 
 import createHashHistory from 'history/createHashHistory';
 const history = createHashHistory(); // Use history.push(...) to programmatically change path, for instance after successfully saving a student
 
-class Menu extends Component {
+
+
+class Login extends Component {
+
   render() {
-    return (
-      <div>
-        <NavLink to="/students">Students</NavLink>
-      </div>
-    );
+    return(
+        <div>
+          <div className="wrapper fadeInDown">
+            <div id="formContent">
+
+              <div className="fadeIn first">
+                <img src="../logo.png" id="icon" alt="User Icon"/>
+                <h3>TurBiz Portal</h3>
+                <h5>AS Sykkelutleie</h5>
+              </div>
+
+              <form>
+                <input type="text" id="login" className="fadeIn second" name="login" placeholder="Brukernavn" />
+                <input type="password" id="password" className="fadeIn third" name="login" placeholder="Passord" />
+                <input type="submit" className="fadeIn fourth" value="Logg inn" onClick={this.validate}/>
+              </form>
+            </div>
+          </div>
+        </div>
+    )
+  }
+
+  validate() {
+    history.push("/home");
   }
 }
 
-class StudentList extends Component {
-  students = [];
+class Home extends Component {
 
   render() {
-    return (
-      <ul>
-        {this.students.map(student => (
-          <li key={student.id}>
-            <NavLink to={'/students/' + student.id + '/edit'}>{student.name}</NavLink>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-
-  mounted() {
-    studentService.getStudents(students => {
-      this.students = students;
-    });
-  }
-}
-
-class StudentEdit extends Component {
-  name = '';
-  email = '';
-
-  render() {
-    return (
-      <form>
-        Name: <input type="text" value={this.name} onChange={event => (this.name = event.target.value)} />
-        Email: <input type="text" value={this.email} onChange={event => (this.email = event.target.value)} />
-        <button type="button" onClick={this.save}>
-          Save
-        </button>
-      </form>
-    );
-  }
-
-  mounted() {
-    studentService.getStudent(this.props.match.params.id, student => {
-      this.name = student.name;
-      this.email = student.email;
-    });
-  }
-
-  save() {
-    studentService.updateStudent(this.props.match.params.id, this.name, this.email, () => {
-      history.push('/students');
-    });
+    return(
+        <div>
+          <h1>Home</h1>
+        </div>
+    )
   }
 }
 
 ReactDOM.render(
-  <HashRouter>
-    <div>
-      <Menu />
-      <Route exact path="/students" component={StudentList} />
-      <Route path="/students/:id/edit" component={StudentEdit} />
-    </div>
-  </HashRouter>,
+    <HashRouter>
+      <div>
+        <Route exact path={"/"} component={Login} />
+        <Route exact path={"/home"} component={Home} />
+      </div>
+    </HashRouter>,
   document.getElementById('root')
 );
