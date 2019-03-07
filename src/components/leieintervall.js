1c38b2acda9c367bf3b3eeb4befaaabeaf41451a
Andreas Thorp <andreas_thorp@hotmail.com>
2019-03-07 14:55:43 +0100

    asasfasf

--- src/components/leieintervall.js
+++ src/components/leieintervall.js
@@ -10,15 +10,18 @@ export class Ordertime extends Component {
   render(){
     return(
       <div>
+      <div>
         <Topnav />
         <select name="place">
           <option value="Sted1">Rallarvegen</option>
           <option value="Sted2">sted 2</option>
           <option value="Sted3">sted 3</option>
         </select>
-
-          <button type="button" id="back" onClick={this.hjem}>Tilbake</button>
-        </div>
+      </div>
+      <div>
+        <button type="button" id="back" className="btn btn-primary btn-mg" onClick={this.hjem}>Tilbake</button>
+      </div>
+    </div>
     )
   }
   hjem() {
