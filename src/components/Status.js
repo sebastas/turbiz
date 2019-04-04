1ba51ed5848046a476ce73d2bcf3cbca8639bd9e
Magnus Vindenes <maggavelli@dhcp-10-22-148-40.wlan.ntnu.no>
2019-04-04 15:45:47 +0200

    Styling og knapper

--- src/components/Status.js
+++ src/components/Status.js
@@ -17,13 +17,17 @@ export class Status extends Component {
   render(){
     return(
       <div className="gradient">
+      <div id="containerlogo">
+      <img src="../src/logo.png" id="icon" alt="User Icon"/>
+      </div>
       <Topnav />
       <div id="regButtons">
+
         <button type="button" id="knapp" className="btn btn-primary btn-lg knapp" onClick={this.sStatus}>Sykler</button>
         <button type="button" id ="knapp" className="btn btn-primary btn-lg knapp" onClick={this.tStatus}>Tilbehør</button>
       </div>
-      <div id="regBack">
-        <Button.Light onClick={this.back}>Tilbake</Button.Light>
+      <div className="regBack">
+        <span className="fa fa-arrow-circle-left fa-3x back"  onClick={this.back}/>
       </div>
       </div>
     )
@@ -109,7 +113,7 @@ export class SyklerStatus extends Component {
   }
 
   goBack() {
-    history.push("/home")
+    history.push("/status")
   }
 
   search(event) {
@@ -186,6 +190,7 @@ export class TilbehorStatus extends Component {
             </Column>
           </Row>
       </div>
+      
     )
   }
 
@@ -196,7 +201,7 @@ export class TilbehorStatus extends Component {
   }
 
   goBack(){
-    history.push("/home")
+    history.push("/status")
   }
 
   search(event) {
