f00961b955cf850deff36bb037ecc4e5d91199eb
sebastas <seb.slettebakken@gmail.com>
2019-04-05 14:09:42 +0200

    home page fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,26 +12,22 @@ export class Home extends Component {
       <div className="gradient">
         <Topnav/>
         <div>
-          <div id="confirmationMsg">
-
           <div id="containerlogo">
-          <img src="../src/logo.png" id="icon" alt="User Icon"/>
-          </div>
-
+            <img src="../src/logo.png" id="icon" alt="User Icon"/>
           </div>
           <div id="container">
-            <button type="button" id="knapp" className="btn btn-primary btn-lg knapp" onClick={this.bestilling} >Bestillingsoversikt</button>
-            <button type="button" id ="knapp" className="btn btn-primary btn-lg knapp" onClick={this.statusUtstyr}>Status utstyr</button>
-            <button type="button" id="knapp" className="btn btn-primary btn-lg knapp" onClick={this.utstyrReg}>Utstyrsregistrering</button>
-          <div>
+            <button type="button" className="btn btn-primary btn-lg knapp" onClick={this.newOrder}>Ny bestilling</button>
+            <button type="button" className="btn btn-primary btn-lg knapp" onClick={this.bestilling}>Bestillingsoversikt</button>
           </div>
           <div id="container2">
-          <button type="button" id="knapp1" className="btn btn-primary btn-lg knapp" onClick={this.employees}
-          style={{display: localStorage.getItem("account") === "admin" ? "" : "none"}}>Ansattoversikt</button>
-          <button type="button" id="knapp1" className="btn btn-primary btn-lg knapp" onClick={this.locations}
-            style={{display: localStorage.getItem("account") === "admin" ? "" : "none"}}>Stedsoversikt</button>
+            <button type="button" className="btn btn-primary btn-lg knapp" onClick={this.statusUtstyr}>Status utstyr</button>
+            <button type="button" className="btn btn-primary btn-lg knapp" onClick={this.utstyrReg}>Utstyrsregistrering</button>
           </div>
-
+          <div id="container3">
+            <button type="button" className="btn btn-primary btn-lg knapp" onClick={this.employees}
+                    style={{display: localStorage.getItem("account") === "admin" ? "" : "none"}}>Ansattoversikt</button>
+            <button type="button" className="btn btn-primary btn-lg knapp" onClick={this.locations}
+                    style={{display: localStorage.getItem("account") === "admin" ? "" : "none"}}>Stedsoversikt</button>
           </div>
         </div>
       </div>
@@ -60,5 +56,8 @@ export class Home extends Component {
   }
   locations() {
     history.push("/locationOverview");
+  }
+  newOrder() {
+    history.push("/order/new/customer")
   }
 }
