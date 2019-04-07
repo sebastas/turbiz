73fe31847afdee2763fdf1398996a67c3e110396
sebastas <seb.slettebakken@gmail.com>
2019-04-07 18:34:23 +0200

    comment user-service

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -2,6 +2,10 @@ import { connection } from '../mysql_connection';
 
 class UserService {
 
+  /**
+   * Gets all users
+   * @param success Callback returns all rows
+   */
   getUsers(success) {
     connection.query('select * from Ansatte', (error, results) => {
       if (error) return console.error(error);
@@ -10,6 +14,11 @@ class UserService {
     });
   }
 
+  /**
+   * Gets specific user from username
+   * @param {string} username Username
+   * @param success Callback returns one row
+   */
   getUser(username, success) {
     connection.query('select * from Ansatte where brukernavn=?', [username], (error, results) => {
       if (error) return console.error(error);
@@ -18,6 +27,12 @@ class UserService {
     });
   }
 
+  /**
+   * Updates user's password
+   * @param {number} id User's id
+   * @param {string} password Chosen password
+   * @param success Callback returns nothing
+   */
   updateUser(id, password, success) {
     connection.query('update Ansatte set passord=? where ansatt_id=?', [password, id], (error, results) => {
       if (error) return console.error(error);
@@ -26,6 +41,15 @@ class UserService {
     });
   }
 
+  /**
+   * Adds a new user to db
+   * @param {string} name User's name
+   * @param {string} email User's email
+   * @param {string} number User's phone number
+   * @param {string} username Appropriate username
+   * @param {string} password Chosen password
+   * @param success Callback returns nothing
+   */
   addUser(name, email, number, username, password, success) {
     let sql = "insert into Ansatte (navn, epost, tlf, brukernavn, passord) values (?, ?, ?, ?, ?)";
     connection.query(sql, [name, email, number, username, password], (error, results) => {
